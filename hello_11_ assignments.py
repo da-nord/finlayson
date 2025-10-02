8bc41dae89fba6e43e253059735a47819a3c7a87
da-nord <keto@xs4all.nl>
2025-10-02 12:51:50 +0200

    Add files via upload

---
+++ hello_11_ assignments.py
@@ -0,0 +1,35 @@
+#11 ASSIGNMENTS
+
+print(" ")
+#111Write a program that prints "Hello, World!" to the console
+print ("hello world")
+
+
+print(" ")
+#112 Ask the user for their name using an input prompt. Then, print a personalized greeting using their name.
+name=input("what's your name?")
+print(f"hallo, {name}")
+
+print(" ")
+#113 Ask the user for two numbers. Print the sum, difference, product, and quotient of those two numbers
+print("please give me two numbers")
+print("i will give you the")
+print("sum")
+print("difference")
+print("product")
+print("quotient")
+
+x=int(input("wat is your first number?"))
+y=int(input("what is your second number"))
+
+print("the sum is:", x+y)
+print("the diffence is:", x-y)
+print("the product is:", x*y)
+print("the quotient is:", x/y)
+#print(x+y)
+
+print(" ")
+#114 Ask the user for their age. Print out how old they will be next year
+x=int(input("what is your age?"))
+print("next year you will be", int(x+1))
+print(" ")
