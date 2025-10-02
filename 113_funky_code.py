cea02d5d901718a1da9562ae180e50b5c509606f
hups <keto@xs4all.nl>
2025-10-02 13:55:47 +0200

    goteborg

---
+++ 113_funky_code.py
@@ -0,0 +1,40 @@
+char = "#"
+
+#de C of CODE
+print(f" {char*4}")
+print(f"{char} ")
+print(f"{char} ")
+print(f"{char} ")
+print(f" {char*4}")
+
+print("")
+
+#de O of CODE
+print(f" {char*4}")
+print(f"{char}    {char} ")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f" {char*4}")
+
+print("")
+
+#de D of CODE
+print(f"{char*3}")
+print(f"{char}   {char}")
+print(f"{char}    {char}")
+print(f"{char}   {char}")
+print(f"{char*3}")
+
+print("")
+
+#de E of CODE
+print(f"{char*4}")
+print(f"{char}")
+print(f"{char*4}")
+print(f"{char}")
+print(f"{char*4}")
+
+print("")
+
+
+
