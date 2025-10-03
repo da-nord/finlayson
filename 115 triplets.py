8368606f669d015c535e066fdd9f453764c14b33
hups <keto@xs4all.nl>
2025-10-03 18:09:23 +0200

    giuseppe

---
+++ 115 triplets.py
@@ -0,0 +1,17 @@
+
+#name1=input("enter the first name:")
+#name1_capitalized=name1.capitalize()
+#name1_formatted=name1.strip().capitalize()
+
+#name2=input("enter the second name:")
+#name2_formatted=name2.strip().capitalize()
+
+#name3=input("enter the third name:")
+#name3_formatted=name3.strip().capitalize()
+
+#print(f"The names are: {name1_formatted},{name2_formatted} and {name3_formatted}")
+
+
+x= input("give me a number to multiply by 10 ").replace(",","") # bij input van getal,000 bv 10,0  31,0  1000,0  wordt dan 10  31  1000
+
+print(int(x) * 10)
