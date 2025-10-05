d21cfdfcfc1a97c223970d11e9eebb65ae182a8d
hups <keto@xs4all.nl>
2025-10-05 12:56:59 +0200

    germans

---
+++ 118_video strings 1.py
@@ -0,0 +1,46 @@
+#video deep dive: python Numbers & mathematics
+# 8:40 import module called math
+import math
+
+print("")
+print ("117 video   deep dive: python Numbers & mathematics")
+print ("did some work along it")
+print("")
+a=3.14
+b=10/3
+c=2+3*4
+d=(2+3)*4
+e=7+2*4-3
+f=1--3
+g=2**2
+h=9//4
+i=2**-2
+j=5%3
+l=math.sqrt(49)
+m=int(math.ceil(5.2))
+
+#sort of menu
+print("a=3.14 just showing a variable with a value")
+print("b=10/3 =",b)
+print("c=2+3*4 =",c," watch the order of operation")
+print("d=(2+3)*4 =",d," expressions inside parenthesis are evaluated first ")
+print("e=7+2*4-3 =",e, " order: multiplication  addition  substraction")
+print("f=1--3 =",f, "dubbele negatief wordt positief 5:26")
+print("g=2**2",g, " machtsverheffen  5:31")
+print("h=9//4 =",h, " integer division rounding up result to next whole number   6:02")
+print("i=2**-2",i, "  6:44")
+print("j=5%3 =",j, " modulus 6:55")
+k=int(input("give number we will determine if it is even " ))
+print(k%2," 0 is even  1 is oneven")
+print("l=sqrt(49)",l," 8:20")
+print("m=int(math.ceil(5.2))  = ",m, "round the number up  9:08  ")
+print("floor is doing the opposite  9:24")
+print("round(number,num_digit)")
+print("round(7.1428,2) = ",round(7.1428,2)," 10:40")
+print("always rounding up to the even number  2.5 to 2  3.5 to 4")
+print("round(7.5) = ",round(7.5)," 11:40")
+me="heinrich"
+leeftijd=91
+print(f"mijn naam is {me} en ik ben {leeftijd} jaar oud")
+print(" ")
+
